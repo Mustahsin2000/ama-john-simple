3eb3732c5d48ce2ae4e9c81c00557f2c1880c449
mustahsin2000 <mustahsin152415@gmail.com>
2022-09-28 06:32:33 -0700

    2nd

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = (props) => {
+    const {cart} = props;
+    let total = 0;
+    let shipping = 0;
+    //cart theke product gula loop korbo er por prottek product er price,shipping,tax niye nibo gula niye nibo )
+    for(const product of cart){ 
+        total = total+product.price;
+        shipping = shipping + product.shipping;
+    }
+    //Tax er jonno:
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total+shipping+parseFloat(tax);
     return (
-        <div>
+        <div className='cart'>
            <h4 className='cart-header'>Order Summery</h4>
-           <p>Selected Items: {props.cart.length}</p> 
+           <p>Selected Items: {cart.length}</p> 
+           <p>Total Price:${total}</p>
+           <p>Total Shipping:${shipping}</p>
+           <p>tax:{tax}</p>
+           <h4>Grand Total:{grandTotal}</h4>
         </div>
     );
 };
